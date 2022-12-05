c86282bb50a24a2494f21a44ec72515c4c29115a
Riva <rivasilvercrown@gmail.com>
2022-12-05 11:08:48 +0100

    added else

--- 2022_day1.py
+++ 2022_day1.py
@@ -1,3 +1,4 @@
+#find the maximum amount of calories the elfs are carrying
 def calory_count(food):
     Calories = 0
     Max_Calories = 0
@@ -9,15 +10,21 @@ def calory_count(food):
                 Max_Calories = Calories
                 print(Max_Calories)
                 Calories = 0
+            else:
+                Calories = 0
     return Max_Calories
 
+#main function
+def __main__():
+    input_file = open("day1input.txt", "r")
+    content = input_file.read()
+    food = content.split("\n")
+    print(calory_count(food))
 
-input_file = open("day1input.txt", "r")
-content = input_file.read()
-food = content.split("\n")
-print(calory_count(food))
+    input_file = open("day1_test.txt", "r")
+    content = input_file.read()
+    food = content.split("\n")
+    print(calory_count(food))
 
-input_file = open("day1_test.txt", "r")
-content = input_file.read()
-food = content.split("\n")
-print(calory_count(food))
+if __name__ == "__main__":
+    __main__()
