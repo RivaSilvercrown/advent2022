854bb437ff9a0cede6e346fb9e1fca977415cb07
Riva <rivasilvercrown@gmail.com>
2022-12-05 14:16:29 +0100

    day 2 second challenge solved

--- 2022_day2.py
+++ 2022_day2.py
@@ -33,6 +33,50 @@ def RPS(strategy):
             
     return score
 
+def RPS_cheat(strategy):
+    #X = lose, Y = win, Z = tie
+    #A = rock, B = paper, C = scissors
+    #rock beats scissors
+    #scissors beats paper
+    #paper beats rock
+    score = 0
+    for i in range(0, len(strategy)):
+        game = strategy[i].split(" ")
+
+        #I need to lose
+        if game[1] == 'X':
+            if game[0] == 'A':
+                #I need to play scissors
+                score = score + 3
+            elif game[0] == 'B':
+                #I need to play rock
+                score = score + 1
+            elif game[0] == 'C':
+                #I need to play paper
+                score = score + 2
+        #I need to draw
+        elif game[1] == 'Y':
+            if game[0] == 'A':
+                #I need to play rock
+                score = score + 1 + 3
+            elif game[0] == 'B':
+                #I need to play paper
+                score = score + 2 + 3
+            elif game[0] == 'C':
+                #I need to play scissors
+                score = score + 3 + 3
+        #I need to win
+        elif game[1] == 'Z':
+            if game[0] == 'A':
+                #I need to play paper
+                score = score + 2 + 6
+            elif game[0] == 'B':
+                #I need to play scissors
+                score = score + 3 + 6
+            elif game[0] == 'C':
+                #I need to play rock
+                score = score + 1 + 6
+    return score
 
 
 def __main__():
@@ -40,6 +84,13 @@ def __main__():
     content = input_file.read()
     strategy = content.split("\n")
     print(RPS(strategy))
+    print(RPS_cheat(strategy))
+
+    input_file = open("day2_test.txt", "r")
+    content = input_file.read()
+    strategy = content.split("\n")
+    print(RPS(strategy))
+    print(RPS_cheat(strategy))
 
 if __name__ == "__main__":
     __main__()
