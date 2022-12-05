35166cf9d658784c5b5b887a28bb40d6ccd0c1b8
Riva <rivasilvercrown@gmail.com>
2022-12-05 10:54:24 +0100

    test

--- 2022_day1.py
+++ 2022_day1.py
@@ -1,10 +1,23 @@
 def calory_count(food):
     Calories = 0
-    print(food)
-    return Calories
+    Max_Calories = 0
+    for f in food:
+        if (f != ''):
+            Calories = Calories + int(f)
+        else:
+            if (Max_Calories < Calories):
+                Max_Calories = Calories
+                print(Max_Calories)
+                Calories = 0
+    return Max_Calories
 
 
 input_file = open("day1input.txt", "r")
+content = input_file.read()
+food = content.split("\n")
+print(calory_count(food))
+
+input_file = open("day1_test.txt", "r")
 content = input_file.read()
 food = content.split("\n")
 print(calory_count(food))
